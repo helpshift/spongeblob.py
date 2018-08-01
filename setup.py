3245b45bb72aeea21e4ed3bf16de2f0552b3d7cd
Ayush Goyal <ayush@helpshift.com>
2018-08-01 12:33:18 +0530

    Bump spongeblob version to 0.0.2
    * Make botocore parameters configurable for boto s3 client
    * Make SSLError a retriable exception
    
    Change-Id: I160a63d5b6b9f03832d61077eb461581387df72b
    JIRA: https://helpshift.atlassian.net/browse/OPS-2311

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='spongeblob',
-      version='0.0.1',
+      version='0.0.2',
       description='Spongeblob: A wrapper library for various cloud storage',
       url='',
       license='Proprietary',
