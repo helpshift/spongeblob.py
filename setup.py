4cfc86f3565064e157de58208b3f9893f3957e54
Ayush Goyal <ayush@helpshift.com>
2018-09-13 18:52:38 +0530

    Add long description in setup.py for pypi, bump version to 0.1.1
    Add long description for the pypi page. Also, bumping bugfix version as pypi
    doesn't support filename reuse.
    
    Change-Id: I80c383b1de05c56a9dd4a771d97278efd95a62d6

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
 setup(name='spongeblob',
-      version='0.1.0',
+      version='0.1.1',
       description='Spongeblob: A wrapper library for various cloud storage',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/helpshift/spongeblob.py',
       license='MIT License',
       packages=find_packages(),
